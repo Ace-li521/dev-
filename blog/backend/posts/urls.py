cb28413a5652250075cf2fb1eba851bfce632418
Ace-li521 <1291256688@qq.com>
2026-01-22 20:53:18 +0800

    添加文章和评论点赞功能

--- blog/backend/posts/urls.py
+++ blog/backend/posts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.PostListView.as_view()),
     path('<int:pk>/', views.PostDetailView.as_view()),
     path('<int:pk>/comments/', views.CommentView.as_view()),
+    path('<int:pk>/like/', views.PostLikeView.as_view()),
+    path('comments/<int:pk>/like/', views.CommentLikeView.as_view()),
     path('upload/', views.UploadImageView.as_view()),
 ]
